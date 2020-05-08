2f46f0f384561b75f3da17192b0461fedb5a5175
Milos Zrnic <zrnkemiki@gmail.com>
2020-05-08 11:40:47 +0200

    init

---
+++ carDiagnostic/src/main/java/com/ftn/carDiagnostic/CarDiagnosticApplication.java
@@ -0,0 +1,13 @@
+package com.ftn.carDiagnostic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDiagnosticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarDiagnosticApplication.class, args);
+	}
+
+}
---
+++ carDiagnostic/src/test/java/com/ftn/carDiagnostic/CarDiagnosticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.carDiagnostic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarDiagnosticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
