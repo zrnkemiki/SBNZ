5d06512bdde9cdeefa0f408a923d8a5c460d473f
lakovicj <31940189+lakovicj@users.noreply.github.com>
2020-07-02 21:15:39 +0200

    end seconds=0

--- carDiagnostic/src/main/java/com/ftn/carDiagnostic/helper/DTOConverter.java
+++ carDiagnostic/src/main/java/com/ftn/carDiagnostic/helper/DTOConverter.java
@@ -84,4 +84,14 @@ public class DTOConverter {
 		
 		return ret;
 	}
+	
+	public static Date setSecondsToZero(Date date) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.MILLISECOND, 0);
+		
+		return cal.getTime();
+	}
 }
